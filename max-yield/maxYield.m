c43676dd3a2ea1a62c3a2f0127a11d6f58e58167
Zak King <zaking17@gmail.com>
2012-09-21 11:23:17 -0700

    updated maxYield to run all outward exchanges

--- max-yield/maxYield.m
+++ max-yield/maxYield.m
@@ -1,218 +1,91 @@
 function maxYield
-
-    run = sprintf('max-yield_rem-loops_glc-4-targets_%s', datestr(now, 'yy-mm-dd_HH_MM_SS'));
-    logFile = [run '.csv'];
-    global fileId dhRxns newNames targetRxns growthMins fluxes swapFluxes
+% maxYield
+% calculate maximum yield of target reactions at minimum biomass production
+% with dehydrogenase swaps
+% 
+% Zachary King 9/20/2012
+    
+    cleaner = onCleanup(@() cleanup);
+    global run status
+    status = 'starting';
+    run = 'max-yield_all-forward-transporters';
+    
+    logFile = sprintf('%s-%s.csv', run, datestr(now, 'yy-mm-dd_HH_MM_SS'));
     fileId = fopen(logFile, 'a');
+    fprintf('%s\n', logFile);
 
+    % load model
     model = loadModelNamed('iJO');
-
-    targetRxns = {% 'EX_etoh(e)'
-                  % 'EX_lac-D(e)'
-                  % 'EX_glyc(e)'
-                  'EX_ala-L(e)'
-                  'EX_pyr(e)'
-                  % 'EX_fum(e)'
-                  % 'EX_succ(e)'
-                  % 'EX_akg(e)',
-                  'EX_for(e)',
-                  'EX_ac(e)',
-                 };
-
-    dhRxns = {'3OAR100'
-              '3OAR40'
-              '3OAR60'
-              '3OAR80'
-              'AGPR'
-              'AKGDH'
-              'ASAD'
-              'DHDPRy'
-              'EAR40x'
-              'EAR60x'
-              'EAR80x'
-              'G6PDH2r'
-              'GAPD'
-              'GLUDy'
-              'GND'
-              'HPYRRx'
-              'HSDy'
-              'ICDHyr'
-              'IPMD'
-              'KARA1'
-              'KARA2'
-              'MDH'
-              'MTHFD'
-              'NADH16pp'
-              'PDH'
-              'PGCD'
-              'SHK3Dr'
-              'TRSARr'
-              'ME1'
-              'ME2'
-              'LDH_D'
-              'LCARR'};
-    % model = makeIrreversible(model,dhRxns);
-
-    biomassRxn = model.rxns(model.c~=0);
-    substrateList = {'EX_glc(e)',
-                     % 'EX_glyc(e)',
-                     % 'EX_xyl-D(e)',
-                    };
-
-    growthMins = [0.0, 0.1, 0.3];
-
+    [model, biomassRxn] = setupModel('iJO','EX_glc(e)', 'anaerobic', 'noTHKO');
+    
+    % get all exchange reactions
+    targetRxns = model.rxns(findExcRxns(model));
+    % turn on special cases
     model = makeFumTransporterReversible(model);
-
+    % remove exchanges that don't exit cell
+    noExitRxns = model.rxns(model.ub <= 0);
+    targetRxns(ismember(targetRxns,noExitRxns)) = [];
+    % set parameters 
+    dhRxns = dhRxnList(31);
+    growthMins = [0.1];
     isAerobic = false;
-    if isAerobic
-        model = changeRxnBounds(model, 'EX_o2(e)', -20, 'l');
-    else
-        model = changeRxnBounds(model, 'EX_o2(e)', 0, 'l');
-    end
+    substrateList = {'EX_glc(e)', 'EX_xyl-D(e)'};
 
-    out = zeros(length(substrateList), length(targetRxns),...
-                length(growthMins), 3);
-    fluxes = cell(size(out)); swapFluxes = fluxes;
-
-    for m = 3:3
-
+    % go!
+    out = zeros(3, length(substrateList), length(targetRxns), length(growthMins));
+    fprintf(fileId, 'swap,substrate,target,growth min,max yield\n'); 
+    for m = 1:3 
         model_K = model;
-
         switch m
           case 1
-            disp('1');
+            fprintf(fileId, 'wildtype,');
           case 2
-            disp('2');
-            % knock out th's
+            fprintf(fileId, 'THKO,');
+            % knock out transhydrogenases
             thRxns = {'NADTRHD', 'THD2pp'};
             model_K = changeRxnBounds(model_K, thRxns, 0, 'b');
           case 3
-            disp('3');
+            fprint(fileId, 'DH-swap,');
             thRxns = {'NADTRHD', 'THD2pp'};
             model_K = changeRxnBounds(model_K, thRxns, 0, 'b');
             [model_K, newNames] = modelSwap(model_K, dhRxns, true);
-            % model_K = makeIrreversible(model_K, newNames);
         end
 
-        for i=1:length(substrateList)
-
-            substrate = substrateList{i};
-
+        for i=1:length(substrateList) 
+            substrate = substrateList{i}; 
+            fprintf(fileId, '%s',substrate);
             model_KS = model_K;
             model_KS = changeRxnBounds(model_KS, 'EX_glc(e)', 0, 'l');
             model_KS = changeRxnBounds(model_KS, substrate, -20, 'l');
 
-            for j = 1:length(targetRxns)
-
+            for j = 1:length(targetRxns) 
+                targetRxn = targetRxns{j};
+                fprintf(fileId, '%s,', targetRxn);
                 model_KST = model_KS;
+                model_KST = setupModelForTarget(model_KST, targetRxn);
                 model_KST.c = zeros(size(model_KST.c));
-                model_KST.c(ismember(model_KST.rxns, targetRxns{j})) = 1;
+                model_KST.c(ismember(model_KST.rxns, targetRxn)) = 1;
+                
+                for k = 1:length(growthMins) 
+                    growthMin = growthMins(k);
+                    fprintf(fileId, '%d,', growthMin);
+                    model_KSTG = model_KST; 
+                    model_KSTG.lb(ismember(model_KSTG.rxns, biomassRxn)) = growthMin;
 
-                for k = 1:length(growthMins)
-
-                    model_KSTG = model_KST;
-
-                    model_KSTG.lb(ismember(model_KSTG.rxns, biomassRxn)) = growthMins(k);
-
-
-                    disp('running optimization with no loops');
-                    changeCobraSolverParams('MILP', 'printLevel', 3); 
-                    sol = optimizeCbModel(model_KSTG,'max',0,0);
-                    if ~isempty(sol.x)
-                        fluxes{i,j,k,m} = sol.x(ismember(model_KSTG.rxns,dhRxns));
-                        if m==3
-                            swapFluxes{i,j,k,m} = sol.x(ismember(model_KSTG.rxns, ...
-                                                                 newNames));
-                        end
-                    else
-                        fluxes{i,j,k,m} = [];
-                    end
-
-                    out(i,j,k,m) = sol.f;
-
-
-                end
-            end
-        end
-
-    end
-    
-    for i=1:length(substrateList)
-        fprintf(fileId, '%s\n\n', substrate)
-        fprintf(fileId, ',0.0,,,0.1,,,0.2,,,0.3\n,');
-        for x=1:4
-            fprintf(fileId, 'noThKO,THKO,THKO+swaps,');
-        end
-        fprintf(fileId, '\n');
-        for j=1:length(targetRxns)
-            fprintf(fileId, '%s,', targetRxns{j});
-            for k=1:length(growthMins)
-                for m=1:3
-                    fprintf(fileId, '%.2f,', out(i,j,k,m));
+                    disp('running optimization');
+                    status = sprintf('%s, %s, min:%d', substrate, targetRxn, growthMin);
+                    % changeCobraSolverParams('MILP', 'printLevel', 3); 
+                    sol = optimizeCbModel(model_KSTG); 
+                    out(m,i,j,k) = sol.f; 
+                    fprintf(fileId, '%.2f,\n', out(m,i,j,k));
                 end
             end
         end
     end
-    
-    fprintf(fileId, '\n\n\nFLUXES\n\n');
-      
-    for i=1:length(substrateList)
-        printSubstrate(substrateList{i},i);
-    end
-
+    status = 'finished'
     fclose(fileId);
-
+    save('raw.mat', 'out');
 end
-
-
-function printSubstrate(substrate,i)
-    global fileId targetRxns
-    fprintf(fileId, '%s\n\n', substrate);
-
-    for j=1:length(targetRxns)
-        printTarget(targetRxns{j},i,j);
-    end
-
-    fprintf(fileId, '\n');
-end
-
-function printTarget(target,i,j)
-    global fileId dhRxns newNames
-    fprintf(fileId, 'target: %s\n', target);
-    fprintf(fileId, ',0.0,,,0.1,,,0.2,,,0.3\n,');
-    for x=1:4
-        fprintf(fileId, 'noThKO,THKO,THKO+swaps,');
-    end
-    fprintf(fileId, '\n');
-    for n=1:length(dhRxns)
-        printDhRxn(dhRxns{n},i,j,n,false);
-    end
-    for n=1:length(newNames)
-        printDhRxn(newNames{n},i,j,n,true);
-    end
-end
-
-function printDhRxn(dhRxn,i,j,n,swap)
-    global fileId growthMins fluxes swapFluxes
-    fprintf(fileId, '%s,', dhRxn);
-    for k=1:length(growthMins)
-        for m=1:3
-            if swap && (m < 3)
-                fprintf(fileId, ',');
-                continue;
-            elseif swap
-                fl = swapFluxes{i,j,k,m};
-            else
-                fl = fluxes{i,j,k,m};
-            end
-            if isempty(fl), fl = 0;
-            else fl = fl(n); end
-            fprintf(fileId, '%.2f,', fl);
-        end
-    end
-    fprintf(fileId, '\n');
-end
-
 
 function model = makeFumTransporterReversible(model)
     transporter = {'FUMt2_2pp'};
@@ -220,10 +93,76 @@ function model = makeFumTransporterReversible(model)
     model.lb(ismember(model.rxns,transporter)) = -1000;
 end
 
-function model = makeIrreversible(model, rxns)
-% make possible loop reactions irreversible
-    if ~isempty(length(rxns))
-        model.rev(ismember(model.rxns,rxns)) = 0;
-        model.lb(ismember(model.rxns,rxns)) = 0;
-    end
-end
+
+
+
+
+                    % if ~isempty(sol.x)
+                        % fluxes{i,j,k,m} = sol.x(ismember(model_KSTG.rxns,dhRxns));
+                        % if m==3
+                        %     swapFluxes{i,j,k,m} = sol.x(ismember(model_KSTG.rxns, ...
+                        %                                          newNames));
+                        % end
+                    % else
+                    %     fluxes{i,j,k,m} = [];
+                    % end 
+
+    
+    % global fileId dhRxns newNames targetRxns growthMins fluxes swapFluxes
+    % fluxes = cell(size(out)); swapFluxes = fluxes;
+    % fprintf(fileId, '\n\n\nFLUXES\n\n');
+      
+    % for i=1:length(substrateList)
+    %     printSubstrate(substrateList{i},i);
+    % end
+
+
+
+
+% function printSubstrate(substrate,i)
+%     global fileId targetRxns
+%     fprintf(fileId, '%s\n\n', substrate);
+
+%     for j=1:length(targetRxns)
+%         printTarget(targetRxns{j},i,j);
+%     end
+
+%     fprintf(fileId, '\n');
+% end
+
+% function printTarget(target,i,j)
+%     global fileId dhRxns newNames
+%     fprintf(fileId, 'target: %s\n', target);
+%     fprintf(fileId, ',0.0,,,0.1,,,0.2,,,0.3\n,');
+%     for x=1:4
+%         fprintf(fileId, 'noThKO,THKO,THKO+swaps,');
+%     end
+%     fprintf(fileId, '\n');
+%     for n=1:length(dhRxns)
+%         printDhRxn(dhRxns{n},i,j,n,false);
+%     end
+%     for n=1:length(newNames)
+%         printDhRxn(newNames{n},i,j,n,true);
+%     end
+% end
+
+% function printDhRxn(dhRxn,i,j,n,swap)
+%     global fileId growthMins fluxes swapFluxes
+%     fprintf(fileId, '%s,', dhRxn);
+%     for k=1:length(growthMins)
+%         for m=1:3
+%             if swap && (m < 3)
+%                 fprintf(fileId, ',');
+%                 continue;
+%             elseif swap
+%                 fl = swapFluxes{i,j,k,m};
+%             else
+%                 fl = fluxes{i,j,k,m};
+%             end
+%             if isempty(fl), fl = 0;
+%             else fl = fl(n); end
+%             fprintf(fileId, '%.2f,', fl);
+%         end
+%     end
+%     fprintf(fileId, '\n');
+% end
