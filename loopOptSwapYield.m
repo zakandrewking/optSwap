2b65d0db5bf05eedd207bec07e9b1b001d6d57df
Zak King <zaking17@gmail.com>
2013-01-16 09:46:38 -0800

    moved d3maps to separate git. added Matlab json

--- loopOptSwapYield.m
+++ loopOptSwapYield.m
@@ -4,7 +4,7 @@ function loopOptSwapYield
     global run status
     status = 'starting';
     run = 'optSwapYield';
-    logFile = sprintf('optSwapYield_%s.txt', ...
+    logFile = sprintf('optSwapYield_no_GAPD_%s.txt', ...
                       datestr(now, 'yy-mm-dd_HH_MM_SS'));
     substrates = {'EX_glc(e)', 'EX_xyl-D(e)'};
     aer = {'anaerobic','aerobic'};
@@ -12,12 +12,15 @@ function loopOptSwapYield
     for i=1:length(substrates)
         for j=1:length(aer)
             for k=1:length(swaps)
+                lTic = tic;
                 opt.substrate = substrates{i};
                 opt.aerobicString = aer{j};
                 opt.swapNum = swaps(k);
                 opt.logFile = logFile;
-                opt.dhRxns = dhRxnList(23);
+                opt.dhRxns = dhRxnList(20);
                 runOptSwapYield(opt);
+                t = toc(lTic);
+                display(sprintf('time: %.1f sec\n', t));
             end
         end
     end
